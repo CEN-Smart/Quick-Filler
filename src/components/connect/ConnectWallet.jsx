e0379ebff8d374dab714fca78526047c88fd8dda
Iceprincess2 <favour.adjushi@decagon.dev>
2024-02-24 19:58:50 +0100

    more edit

--- src/components/connect/ConnectWallet.jsx
+++ src/components/connect/ConnectWallet.jsx
@@ -1,67 +1,66 @@
-import React, {useState, useEffect} from 'react'
-import { Xumm } from 'xumm'
+import React, { useState, useEffect } from "react";
+import { Xumm } from "xumm";
 
-
-const xumm = new Xumm("8d01558e-d605-4165-8a35-c0a8763800a8") 
+const xumm = new Xumm("8d01558e-d605-4165-8a35-c0a8763800a8");
 
 function ConnectWallet() {
-  const [account, setAccount] = useState('')
+  const [account, setAccount] = useState("");
 
-useEffect(()=>{
-  async function output() {
+  useEffect(() => {
+    async function output() {
+      if (window.localStorage.getItem("xumm_address") !== null) {
+        const _account = localStorage.getItem("xumm_address");
+        setAccount(_account);
+        console.log("account on first render is ", account);
+      }
+    }
+    output();
+  }, []);
+
+  xumm.user.account.then((a) => {
+    setAccount(a ?? "");
+    console.log("about to set account");
+    localStorage.setItem("xumm_address", account);
+  });
+
+  const logout = () => {
+    xumm.logout();
+    setAccount("");
+    login();
+  };
+  const login = async () => {
     if (window.localStorage.getItem("xumm_address") !== null) {
-      const _account = localStorage.getItem('xumm_address');
-      setAccount(_account)
-      console.log("account on first render is ", account)
+      const _account = localStorage.getItem("xumm_address");
+      console.log(_account);
+      if (_account.length == 0) {
+        xumm.authorize();
+      }
+      setAccount(_account);
+    } else {
+      await xumm.authorize();
     }
-  } output()
-}, [])
-
-
-
-xumm.user.account.then(a => {
-  setAccount(a ?? '')
-  console.log("about to set account")
-  localStorage.setItem('xumm_address', account);
-})
-
-
-const logout = () => {
-  localStorage.removeItem("xumm_address");
-  setAccount('');
-}
-const login = async () => {
-  if (window.localStorage.getItem("xumm_address") !== null) {
-    const _account = localStorage.getItem('xumm_address');
-    console.log(_account)
-    if (_account.length == 0) {
-      xumm.authorize()
-    }
-    setAccount(_account)
-  } else {
-    await xumm.authorize()
-  }
-}
-  
-
+  };
 
   return (
     <div>
-              {
-        account === '' && !xumm.runtime.xapp
-          ? <button onClick={login} className="button">Connect Wallet</button>
-          : ''
-      }
-      {
-        account !== ''
-          ? <>
-              <button onClick={logout} className="button">Disconnect</button>
-            </>
-          : ''
-        }
-
+      {account === "" && !xumm.runtime.xapp ? (
+        <button onClick={login} className="button">
+          Connect Wallet
+        </button>
+      ) : (
+        ""
+      )}
+      {account !== "" ? (
+        <>
+          <button onClick={logout} className="button">
+            Disconnect
+          </button>
+        </>
+      ) : (
+        ""
+      )}
     </div>
-  )
+  );
 }
 
-export default ConnectWallet
+export default ConnectWallet;
