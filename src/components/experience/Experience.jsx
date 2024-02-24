b1f6ad21b640f29120b9392873d7207b0d69547a
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2024-02-24 20:03:56 +0100

    redesign the cylinder section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,31 +1,29 @@
-import React from "react";
-import "./Experience.css";
-import Gas from "../../assets/cooking-gas.jpeg";
-import ConnectWallet from "../connect/ConnectWallet";
+import './Experience.css';
+import Gas from '../../assets/cooking-gas.jpeg';
+import ConnectWallet from '../connect/ConnectWallet';
 
 function Experience() {
   return (
-    <div className="jumbotron">
-      <div className="text-container">
-        <p className="style">
-          <span className="future">
-            Experience the <br />
-          </span>
-          <span className="">Future of gas filling</span>
-        </p>
-        <div className="filler">
-          <p>
-            Quick filler revolutionize gas filling with secure online transaction <br/> and
-            XRP cryptocurrency integration
+    <div className='experience__container'>
+      <div className='experience__content'>
+        <div className='experience__content--left'>
+          <h1 className='experience__content--title'>
+            Experience the Future of gas filling
+          </h1>
+          <p className='experience__content--text'>
+            Quick filler revolutionize gas filling with secure online
+            transactions and XRP cryptocurrency integration.
           </p>
+          <div className='experience__content--connect'>
+            <a href='learn-more' target='_blank' rel='noreferrer'>
+              Learn More
+            </a>
+            <ConnectWallet />
+          </div>
         </div>
-        <div className="button-container">
-          <button className="learn">Learn more</button>
-          <ConnectWallet />      
-    </div>
-      </div>
-      <div className="cylinder">
-        <img src={Gas} alt="gas" className="pic" />
+        <div className='experience__content--right'>
+          <img src={Gas} alt='Cooking Gas' />
+        </div>
       </div>
     </div>
   );
