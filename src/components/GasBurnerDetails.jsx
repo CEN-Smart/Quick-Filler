73ee085f07e16f4c45abf000bef8a64b97bd2c52
Chikodinaka Ann Anyanwu <chikodiann@gmail.com>
2024-05-07 12:18:35 +0100

    burner

--- src/components/GasBurnerDetails.jsx
+++ src/components/GasBurnerDetails.jsx
@@ -114,7 +114,7 @@ function GasBurnerDetails() {
 									))}
 								</div>
 								<div className='flex flex-col gap-2 border-t border-gray-600'>
-									<h3 className='mt-4'>Select Kg</h3>
+									<h3 className='mt-4'>Select Type</h3>
 									<p>Input delivery address</p>
 								</div>
 								<div className='flex flex-row items-center justify-center mt-4'>
